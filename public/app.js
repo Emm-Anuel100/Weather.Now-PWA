8266ad2ba637363fd16c3dc583846877b7260928
Emm-Anuel100 <chinweokwuemmanuel2004@gmail.com>
2024-05-30 10:17:32 +0100

    Recent update

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 
-//*** register service worker****//
+//*** register service worker ****//
 if ('serviceWorker' in navigator) {
    window.addEventListener('load', () => {
        navigator.serviceWorker.register('/service-worker.js')
